58600407f915638309a8ecd7a234b8a75d00f8c8
ali <140660389+alielsharqawy@users.noreply.github.com>
2025-02-02 22:51:18 +0200

    nav-color

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -24,25 +24,25 @@ function Navbar() {
   return (
     <nav className="bg-emerald-500 dark:bg-gray-900 text-white transition-all py-4 px-3 shadow-xl flex justify-between items-center">
       <div className="container mx-auto flex justify-between items-center">
-        <div className="text-xl font-bold">
+        <div className="text-xl font-bold text-white">
           <Link to="/" onClick={handleLinkClick}>
             {t("navbar.brand_name")}
           </Link>
         </div>
         <div className="flex justify-center items-center gap-3">
           <ul
-            className={`md:flex md:space-x-6 absolute md:static left-0 top-16 w-full md:w-auto md:bg-transparent p-4 space-y-4 md:space-y-0 transition-all duration-300 ${
+            className={`md:flex md:space-x-6 absolute md:static left-0 top-16 w-full md:w-auto bg-emerald-500 dark:bg-gray-900 text-white md:bg-transparent p-4 space-y-4 md:space-y-0 transition-all duration-300 ${
               isOpen ? "block" : "hidden"
             }`}
           >
-            <li className="text-lg font-medium cursor-pointer hover:text-orange-500 px-2">
+            <li className="text-lg font-medium cursor-pointer hover:text-orange-500">
               <Link to="/" onClick={handleLinkClick}>
                 {t("navbar.home")}
               </Link>
             </li>
             <li className="relative">
               <button
-                className="text-lg font-medium flex items-center cursor-pointer bg-emerald-500 dark:bg-gray-900 text-white hover:text-orange-500"
+                className="text-lg font-medium flex items-center cursor-pointer hover:text-orange-500"
                 onClick={() => setIsDietsOpen(!isDietsOpen)}
               >
                 {t("navbar.diets")} <ChevronDown size={16} className="ml-1" />
